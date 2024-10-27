590daa98fee2432689ceec663c209c938efbd527
V-Yashasvi <vyashasvi1@gmail.com>
2024-10-27 21:24:20 +0530

    first commit

---
+++ Paytm Payment/script.js
@@ -0,0 +1,42 @@
+
+let paymentBtn=document.getElementById("pay-button")
+let paymentContainer=document.getElementById('payment-container')
+let phoneContainer=document.getElementById('phone-container')
+let postPayment=document.getElementById('post-payment')
+
+
+paymentBtn.addEventListener('click',()=>{
+    let statusDiv=document.getElementById('status')
+
+    // paymentContainer.style.display='none';
+    // postPayment.style.display='block';
+
+    // postPayment.appendChild(statusDiv);
+
+    window.location.href='./next.html'
+    statusDiv.style.backgroundColor='red'
+
+    statusDiv.className='processing';
+    statusDiv.innerText='Processing.....';
+
+    processPayment().then((message)=>{
+        statusDiv.className='success'
+        statusDiv.innerText=message
+    }).catch((error)=>{
+        statusDiv.className='failure'
+        statusDiv.innerText=error
+    })
+})
+
+function processPayment(){
+    return new Promise((resolve,reject)=>{
+        setInterval(()=>{
+            const randomSucess=Math.random()>0.5;
+            if (randomSucess){
+                resolve("Payment is Successful!...")
+            }else{
+                reject("Could'nt complete Payment.Try Again!")
+            }
+        },2000)
+    })
+}
